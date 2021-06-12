8d94d386624f5adf7ace12d57dd4e6190ab42465
suman <sumandari.ee@gmail.com>
2021-06-12 19:41:39 +0800

    deleted unit test

--- app/quotations/tests.py
+++ app/quotations/tests.py
@@ -23,37 +23,5 @@ class TestQuotationModel(TestCase):
             quot.vehicle_year, 2000
         )
         self.assertEqual(
-            quot.cov_windscreen, 'no'
-        )
-        self.assertEqual(
-            quot.cov_passanger_liability, 'no'
-        )
-        self.assertEqual(
-            quot.cov_others, 'no'
-        )
-        self.assertEqual(
             quot.price, 1234.56
-        )
-
-    def test_create_quotation_with_addon(self):
-        quot = Quotation.objects.create(
-            price=1234.56,
-            name='Customer Test',
-            email='email@example.com',
-            mobile_number='123456789',
-            vehicle_year=2000,
-            vehicle_model='Savvy',
-            vehicle_number='CAR12345',
-            vehicle_price=123456.78,
-            cov_windscreen='yes',
-            cov_others='yes'
-        )
-        self.assertEqual(
-            quot.cov_windscreen, 'yes'
-        )
-        self.assertEqual(
-            quot.cov_passanger_liability, 'no'
-        )
-        self.assertEqual(
-            quot.cov_others, 'yes'
         )
